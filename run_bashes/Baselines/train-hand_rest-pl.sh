539d8529debe5e39b538f75859124ee89483ae3d
Alexander-Jing <18061523086@163.com>
2025-02-20 23:27:44 +0800

    bashes added for hand-rest training, 2025.2.20

---
+++ run_bashes/Baselines/train-hand_rest-pl.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define the workspace folder
+workspace_folder="/home/jyt/workspace/DeepTransferBCI"
+
+# Change the current directory to the workspace folder
+cd $workspace_folder
+
+# Define the program path
+program_path="tl/pl.py"
+
+# Set the PYTHONPATH environment variable
+export PYTHONPATH="${PYTHONPATH}:$(pwd)"
+
+# Execute the Python script with the specified arguments
+python3 $program_path \
+  --dataset_name "MI-hand_rest" \
+  --data_save "True" \
+  --data_path "./data/" \
+  --data_path_MI "/home/jyt/workspace/transfer_models/datasets_MI/hand_elbow/derivatives" \
+  --log_path "./logs/Pl-hand-rest-momentum-cls/" \
+  --use_pretrained_model "True" \
+  --finetune "False" \
+  --momentum "False" \
+  --momentum_param "0.5"
--- tl/bn-adapt.py
+++ tl/bn-adapt.py
@@ -406,7 +406,7 @@ if __name__ == '__main__':
         total_acc = []
 
         # update multiple models, independently, from the source models
-        for s in [2, 3, 4, 5]:
+        for s in [3, 4, 5]:
             args.SEED = s
 
             fix_random_seed(args.SEED)
