ec3a5e058c53b1a9cd7600b0d78ec31dd4ec7650
Alexander-Jing <18061523086@163.com>
2026-02-18 20:56:20 +0800

    UDA baselines implemented, version 2026.2.17, 2026.2.18

--- tl/UDA/mcc.py
+++ tl/UDA/mcc.py
@@ -11,11 +11,12 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import pandas as pd
-from utils.network import backbone_net
-from utils.LogRecord import LogRecord
-from utils.dataloader import read_mi_combine_tar
-from utils.utils import lr_scheduler_full, fix_random_seed, cal_acc_comb, data_loader, cal_auc_comb
-from utils.loss import ClassConfusionLoss
+from tl.utils.network import backbone_net
+from tl.utils.LogRecord import LogRecord
+from tl.utils.dataloader import read_mi_combine_tar
+from tl.utils.utils import lr_scheduler_full, fix_random_seed, cal_acc_comb, data_loader, cal_auc_comb
+from tl.utils.loss import ClassConfusionLoss
+from tl.utils.utils import float_list, str2bool
 
 import gc
 import sys
@@ -99,44 +100,172 @@ def train_target(args):
 
 
 if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_name', type=str, default='BNCI2014001', help='the data set name, now support BNCI2014001, BNCI2014002, BNCI2015001 from moabb')
+    parser.add_argument('--data_save', type=str2bool, default=True, help='whether save the data to file')
+    parser.add_argument('--data_path', type=str, default='./data/', help='the path to save the data from mobba dataset')
+    parser.add_argument('--data_path_MI', type=str, default='/home/jyt/workspace/transfer_models/datasets_MI/hand_elbow/derivatives', help='the path to save the data from other datasets')
+    parser.add_argument('--log_path', type=str, default='./logs/', help='the path to save the logs')
+    parser.add_argument('--gpu_idx', type=int, default=0, help='index of GPU')
+    parser.add_argument('--use_pretrained_model', type=str2bool, default=False, help='whether to use the pretrained model parameters')
+    parser.add_argument('--finetune', type=str2bool, default=False, help='whether to finetune the model with part of the target data')
+    parser.add_argument('--ft_volume', type=int, default=7*40, help='the amount of data for finetuning in target domain')
+    parser.add_argument('--momentum', type=str2bool, default=False, help='whether to use the momentum updating for model parameters')
+    parser.add_argument('--momentum_param', type=float, default=0.5, help='the value for momentum updating')
+    parser.add_argument('--align', type=str2bool, default=True, help='use EA alignment and IEA alignment')
+    parser.add_argument('--batch_size', type=int, default=32, help='batch size in offline training')
+    parser.add_argument('--batch_size_online', type=int, default=8, help='batch size in online adaptation')
+    parser.add_argument('--stride', type=int, default=1, help='stride in online adaptation')
+    parser.add_argument('--lr', type=float, default=0.001, help='learning rate in offline and online training')
+    parser.add_argument('--lr_online', type=float, default=0.001, help='learning rate in online adaptation')
+    parser.add_argument('--epoch', type=int, default=100, help='epoches in offline and online training')
+    parser.add_argument('--backbone', type=str, default='EEGNet', help='backbone of the model')
+    parser.add_argument('--param_runs', type=str, default='./runs/', help='folder for saving the run paramters')
+    parser.add_argument('--use_BN', type=str2bool, default=True, help='whether to only use BN adaptation')
+    parser.add_argument('--loss_func', type=str, default="MemorySoftplusEnergyWeightedAlignment", help='the name of loss function')
+    parser.add_argument('--updating_type', type=str, default="entropy", help='updating type')
+    parser.add_argument('--selection_ratio', type=float, default=0.5, help='the ratio for sample selection in EnergyEntropy_selected')
+    parser.add_argument('--selection_ratio_review', type=float, default=0.5, help='the ratio for review sample selection in EnergyEntropy_selected')
+    parser.add_argument('--mt', type=float, default=0.9, help='the momentum value for teacher model')
+    parser.add_argument('--loss_weights', type=float_list, default=[1.0, 1.0, 1.0], help='weights for 3 loss components')
+    parser.add_argument('--scale', type=float, default=5.0, help='the scale value for ConsSamples_selection_two_stage_weighted_4')
+    parser.add_argument('--confidence_threshold', type=float, default=0.75, help='threshold for high-confidence sample selection')
+    parser.add_argument('--entropy_threshold', type=float, default=0.6, help='threshold for low-entropy sample selection')
+    parser.add_argument('--calibrate_probs', type=str2bool, default=False, help='use CalibratedPseudoLabels for calibration')
+    parser.add_argument('--memory_type', type=str, default='DropMemoryBank_review', help='type of memory buffer')
+    parser.add_argument('--memory_review', type=str, default='get_memory_review', help='type of memory review')
+    parser.add_argument('--weight_type', type=str, default='entropy_energy', help='type of weights for sample selection')
+    parser.add_argument('--memory_capacity', type=int, default=64, help='capacity for the memory buffer')
+    parser.add_argument('--thre_alpha', type=float, default=1.0, help='thre_alpha * threshold, the scale factor for dynamic threshold')
+    parser.add_argument('--loss_weight_type', type=str, default='sigmoid', help='type of weights for two-satge updating')
+    parser.add_argument('--gate_type', type=str, default='mean', help='type of weights for gating')
+    parser.add_argument('--buffer_selefction_type', type=str, default='confidence', help='type of weights for buffer selection')
+    parser.add_argument('--min_threshold', type=float, default=0.40, help='minimum threshold for dynamic thresholding')
+    parser.add_argument('--warm_up', type=str, default='capacity', help='type of warm up for memory buffer')
+    parser.add_argument('--temp', type=float, default=1.0, help='temprature for sharpening in the constrastive loss')
+    parser.add_argument('--two_stage', type=str2bool, default=True, help='update the model in a two stage form')
+    parser.add_argument('--save_results', type=str2bool, default=False, help='whether to save results for visulization')
+    parser.add_argument('--save_results_two_stage', type=str2bool, default=False, help='whether to save results of two stage for visulization')
+    parser.add_argument('--grad_visual', type=str2bool, default=False, help='whether to save results of grads for visulization')
+    
+    args_parser = parser.parse_args()
 
-    data_name_list = ['BNCI2014001', 'BNCI2014002', 'BNCI2015001']
+    data_name = args_parser.dataset_name
+    data_save = args_parser.data_save
+    data_path = args_parser.data_path
+    data_path_MI = args_parser.data_path_MI
+    log_path = args_parser.log_path
+    gpu_idx = args_parser.gpu_idx
+    use_pretrained_model = args_parser.use_pretrained_model
+    finetune = args_parser.finetune
+    ft_volume = args_parser.ft_volume
+    momentum = args_parser.momentum
+    momentum_param = args_parser.momentum_param
+    align = args_parser.align
+    batch_size = args_parser.batch_size
+    batch_size_online = args_parser.batch_size_online
+    lr = args_parser.lr
+    epoch = args_parser.epoch
+    backbone = args_parser.backbone
+    param_runs = args_parser.param_runs
+    lr_online = args_parser.lr_online
+    use_BN = args_parser.use_BN
+    stride = args_parser.stride
+    loss_func = args_parser.loss_func
+    updating_type = args_parser.updating_type
+    selection_ratio = args_parser.selection_ratio
+    mt = args_parser.mt
+    loss_weights = args_parser.loss_weights
+    scale =args_parser.scale
+    confidence_threshold = args_parser.confidence_threshold
+    entropy_threshold = args_parser.entropy_threshold
+    calibrate_probs = args_parser.calibrate_probs
+    memory_type = args_parser.memory_type
+    memory_review = args_parser.memory_review
+    weight_type = args_parser.weight_type
+    memory_capacity = args_parser.memory_capacity
+    selection_ratio_review = args_parser.selection_ratio_review
+    thre_alpha = args_parser.thre_alpha
+    loss_weight_type = args_parser.loss_weight_type
+    gate_type = args_parser.gate_type
+    buffer_selefction_type = args_parser.buffer_selefction_type
+    min_threshold = args_parser.min_threshold
+    warm_up = args_parser.warm_up
+    temp = args_parser.temp
+    two_stage = args_parser.two_stage
+    save_results = args_parser.save_results
+    save_results_two_stage = args_parser.save_results_two_stage
+    grad_visual = args_parser.grad_visual
 
+    print('dataset_name: {}, type: {}'.format(data_name, type(data_name)))
+    print('data_save: {}, type: {}'.format(data_save, type(data_save)))
+    print('data_path_MI: {}, type: {}'.format(data_path_MI, type(data_path_MI)))
+    print('data_path: {}, type: {}'.format(data_path, type(data_path)))
+    print('log_path: {}, type: {}'.format(log_path, type(log_path)))
+    print('gpu_idx: {}, type: {}'.format(gpu_idx, type(gpu_idx)))
+
+    data_name_list = ['BNCI2014001', 'BNCI2014002', 'BNCI2015001', 'BNCI2014001-4', 'MI-hand_elbow','MI-elbow_rest', 'MI-hand_rest', 
+                      'BNCI2014001-4-all', 'BNCI2014001-4-test', 'BNCI2014001-4-train', 'BNCI2014_004-train', 'BNCI2014_004-test',
+                      'WBCIC-SHU-3C']
     dct = pd.DataFrame(
         columns=['dataset', 'avg', 'std', 's0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11',
                  's12', 's13'])
 
-    for data_name in data_name_list:
-
-        if data_name == 'BNCI2014001': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 22, 2, 1001, 250, 144, 248
-        if data_name == 'BNCI2014002': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 14, 15, 2, 2561, 512, 100, 640
-        if data_name == 'BNCI2015001': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 12, 13, 2, 2561, 512, 200, 640
-
+    if data_name in data_name_list:
+        # N: number of subjects, chn: number of channels
+        if backbone == 'EEGNet':
+            if data_name == 'BNCI2014001': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 22, 2, 1001, 250, 144, 248
+            if data_name == 'BNCI2014002': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 14, 15, 2, 2561, 512, 100, 640
+            if data_name == 'BNCI2015001': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 12, 13, 2, 2561, 512, 200, 640
+            if data_name == 'BNCI2014001-4': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 22, 4, 1001, 250, 576, 496
+            if data_name == 'BNCI2014001-4-train': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 22, 4, 1001, 250, 288, 496
+            if data_name == 'MI-hand_elbow': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 25, 62, 2, 800, 200, 600, 200
+            if data_name == 'MI-elbow_rest': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 25, 62, 2, 800, 200, 600, 200
+            if data_name == 'MI-hand_rest': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 25, 62, 2, 800, 200, 600, 200
+            if data_name == 'BNCI2014001-4-all': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 22, 4, 1001, 250, 576, 496
+            if data_name == 'BNCI2014001-4-test': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 22, 4, 1001, 250, 288, 496
+            if data_name == 'BNCI2014_004-train': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 3, 2, 1126, 250, 400, 560
+            if data_name == 'BNCI2014_004-test': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 3, 2, 1126, 250, 400, 560
+            if data_name == 'WBCIC-SHU-3C': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 11, 58, 3, 1000, 250, 900, 496
+        if backbone == 'EEGNet-4,2':
+            if data_name == 'BNCI2014001-4-train': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 22, 4, 1001, 250, 288, 248
+            if data_name == 'MI-hand_elbow': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 25, 62, 2, 800, 200, 600, 200
+            if data_name == 'MI-elbow_rest': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 25, 62, 2, 800, 200, 600, 200
+            if data_name == 'MI-hand_rest': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 25, 62, 2, 800, 200, 600, 200
+            if data_name == 'BNCI2014001-4-all': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 22, 4, 1001, 250, 576, 248
+            if data_name == 'BNCI2014001-4-test': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 22, 4, 1001, 250, 288, 248
+            if data_name == 'BNCI2014_004-train': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 3, 2, 1126, 250, 400, 280
+            if data_name == 'BNCI2014_004-test': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 9, 3, 2, 1126, 250, 400, 280
+            if data_name == 'WBCIC-SHU-3C': paradigm, N, chn, class_num, time_sample_num, sample_rate, trial_num, feature_deep_dim = 'MI', 11, 58, 3, 1000, 250, 900, 248
+        
         args = argparse.Namespace(feature_deep_dim=feature_deep_dim, trial_num=trial_num,
                                   time_sample_num=time_sample_num, sample_rate=sample_rate,
                                   N=N, chn=chn, class_num=class_num, paradigm=paradigm)
 
         args.method = 'MCC'
-        args.backbone = 'EEGNet'
+        args.backbone = backbone
 
         # whether to use EA
-        args.align = True
+        args.align = align
 
         # learning rate
-        args.lr = 0.001
+        args.lr = lr
 
         # train batch size
-        args.batch_size = 32
+        args.batch_size = batch_size
         if paradigm == 'ERP':
             args.batch_size = 256
 
         # training epochs
-        args.max_epoch = 100
+        args.max_epoch = epoch
+
+        args.finetune = finetune
 
         # GPU device id
         try:
-            device_id = str(sys.argv[1])
-            os.environ["CUDA_VISIBLE_DEVICES"] = device_id
+            device_id = gpu_idx
+            os.environ["CUDA_VISIBLE_DEVICES"] = str(device_id)
             args.data_env = 'gpu' if torch.cuda.device_count() != 0 else 'local'
         except:
             args.data_env = 'local'
@@ -155,14 +284,15 @@ if __name__ == '__main__':
             print(args.SEED)
             print(args)
 
-            args.local_dir = './data/' + str(data_name) + '/'
-            args.result_dir = './logs/'
+            args.local_dir = data_path + str(data_name) + '/'
+            args.result_dir = log_path
             my_log = LogRecord(args)
             my_log.log_init()
             my_log.record('=' * 50 + '\n' + os.path.basename(__file__) + '\n' + '=' * 50)
 
             sub_acc_all = np.zeros(N)
             for idt in range(N):
+                fix_random_seed(args.SEED)  # fix the seed
                 args.idt = idt
                 source_str = 'Except_S' + str(idt)
                 target_str = 'S' + str(idt)
@@ -208,4 +338,4 @@ if __name__ == '__main__':
         dct = dct.append(result_dct, ignore_index=True)
 
     # save results to csv
-    dct.to_csv('./logs/' + str(args.method) + ".csv")
+    dct.to_csv(os.path.join(log_path, str(args.method) + ".csv"))
